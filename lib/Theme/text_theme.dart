8b8d6426075c2be37e5fcbc99d3b529686e789ef
ManagedStudios <dibbomrinmoy@hotmail.com>
2023-08-27 17:24:47 +0200

    text theme

---
+++ lib/Theme/text_theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const textTheme = TextTheme(
+    bodyLarge: TextStyle(
+        fontSize: 20, fontWeight: FontWeight.w600, letterSpacing: 0.1),
+    labelSmall:
+        TextStyle(fontSize: 14, fontWeight: FontWeight.w300, letterSpacing: 0),
+    bodyMedium: TextStyle(
+        fontSize: 20, fontWeight: FontWeight.w500, letterSpacing: -0.1),
+    labelMedium:
+        TextStyle(fontSize: 20, fontWeight: FontWeight.w300, letterSpacing: 0));
+// 
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:buecherteam_2023_desktop/Theme/color_scheme.dart';
+import 'package:buecherteam_2023_desktop/Theme/text_theme.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,9 +15,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: lightColorScheme,
-        useMaterial3: true,
-      ),
+          colorScheme: lightColorScheme,
+          useMaterial3: true,
+          fontFamily: 'Helvetica Neue',
+          textTheme: textTheme),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -42,27 +44,22 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
         title: Text(widget.title),
       ),
       body: Center(
-
         child: Column(
-
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Text(
+              'Wieland Siegward',
+              style: Theme.of(context).textTheme.labelSmall,
             ),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-
+              style: Theme.of(context).textTheme.bodyLarge,
             ),
           ],
         ),
