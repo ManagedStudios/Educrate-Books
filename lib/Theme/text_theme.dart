c59b112081fec6bd35361a1b18b61289ba25ec00
ManagedStudios <dibbomrinmoy@hotmail.com>
2023-08-30 16:32:06 +0200

    made font a bit smaller

--- lib/Theme/text_theme.dart
+++ lib/Theme/text_theme.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 
 const textTheme = TextTheme(
     bodyLarge: TextStyle(
-        fontSize: 20, fontWeight: FontWeight.w600, letterSpacing: 0.1),
+        fontSize: 18, fontWeight: FontWeight.w600, letterSpacing: 0.1),
     labelSmall:
-        TextStyle(fontSize: 14, fontWeight: FontWeight.w300, letterSpacing: 0),
+        TextStyle(fontSize: 12, fontWeight: FontWeight.w300, letterSpacing: 0),
     bodyMedium: TextStyle(
-        fontSize: 20, fontWeight: FontWeight.w500, letterSpacing: -0.1),
+        fontSize: 18, fontWeight: FontWeight.w500, letterSpacing: -0.1),
     labelMedium:
-        TextStyle(fontSize: 20, fontWeight: FontWeight.w300, letterSpacing: 0));
+        TextStyle(fontSize: 18, fontWeight: FontWeight.w300, letterSpacing: 0));
 // 
