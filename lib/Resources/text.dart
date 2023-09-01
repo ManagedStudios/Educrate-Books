0dda605d3dab53abac763cf4afc3d34fdba7b4e9
ManagedStudios <dibbomrinmoy@hotmail.com>
2023-09-01 19:15:09 +0200

    updatedText

---
+++ lib/Resources/text.dart
@@ -0,0 +1,11 @@
+
+ class TextRes {
+  static const String dbName = "lfgBooks";
+
+  static const String dbAccesError = "Datenbankinteraktion fehlgeschlagen: ";
+
+  static const String timeOutException = "Ladefehler: ";
+
+  static const String unexpectedDbError = "Ein unerwarteter Fehler bei der Datenbankinteraktion: ";
+
+ }
