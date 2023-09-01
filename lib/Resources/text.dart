31bf00ecbacf937bc1fc8b1823ea5bac8e55d89b
ManagedStudios <dibbomrinmoy@hotmail.com>
2023-09-01 22:38:54 +0200

    string templates for json fields of student and bookLite class

--- lib/Resources/text.dart
+++ lib/Resources/text.dart
@@ -8,4 +8,18 @@
 
   static const String unexpectedDbError = "Ein unerwarteter Fehler bei der Datenbankinteraktion: ";
 
+  static const String studentIdJson = 'id';
+  static const String studentFirstNameJson = 'firstName';
+  static const String studentLastNameJson = 'lastName';
+  static const String studentClassLevelJson = TextRes.bookClassLevelJson;
+  static const String studentClassCharJson = 'classChar';
+  static const String studentTrainingDirectionsJson = 'trainingDirections';
+  static const String studentBooksJson = 'books';
+  
+  static const String bookIdJson = 'bookId';
+  static const String bookNameJson = 'name';
+  static const String bookSubjectJson = 'subject';
+  static const String bookClassLevelJson = 'classLevel';
+  
+
  }
