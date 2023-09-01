862853266aefe1c4c031ce6da7d1da9dd378317b
ManagedStudios <dibbomrinmoy@hotmail.com>
2023-09-01 22:43:19 +0200

    Filter class passed as argument in the streamStudents method

---
+++ lib/Data/filter.dart
@@ -0,0 +1,10 @@
+
+class Filter {
+  List<MapEntry<int, String>>? filteredClasses;
+  int? amountOfOwnedBooks;
+  bool? hasBooks;
+  List<String>? filteredTrainingDirections;
+  List<String>? filteredBookIds;
+
+
+}
