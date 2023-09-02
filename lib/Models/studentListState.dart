a64b5528d2d737d8512b5bbbf2ce2aa4f76d4a58
ManagedStudios <dibbomrinmoy@hotmail.com>
2023-09-02 15:47:32 +0200

    stream students, save students and delete Students

--- lib/Models/studentListState.dart
+++ lib/Models/studentListState.dart
@@ -1,11 +1,12 @@
 
+import 'dart:async';
 import 'dart:collection';
 
 
+import 'package:buecherteam_2023_desktop/Data/buildQuery.dart';
 import 'package:buecherteam_2023_desktop/Data/db.dart';
 import 'package:buecherteam_2023_desktop/Data/filter.dart';
 import 'package:buecherteam_2023_desktop/Data/student.dart';
-import 'package:buecherteam_2023_desktop/Resources/text.dart';
 import 'package:cbl/cbl.dart';
 import 'package:flutter/material.dart';
 
@@ -19,27 +20,44 @@ class StudentListState extends ChangeNotifier {
   HashSet<String> selectedStudentIds = HashSet();
 
   /*
-  saveStudent to save completely new Students
+  saveStudent saves completely new Students
    */
   Future<void> saveStudent(String firstName, String lastName, int classLevel,
       String classChar, List<String> trainingDirections) async {
+    final document = createNewStudentDoc(firstName, lastName, classLevel, classChar, trainingDirections);
+    await database.saveDocument(document);
+  }
+
+  /*
+  createNewStudentDoc creates the appropriate document for the saveStudent method
+  the arguments are validated in the Stateful form widget
+   */
+  MutableDocument createNewStudentDoc (String firstName, String lastName, int classLevel,
+      String classChar, List<String> trainingDirections) {
     final document = MutableDocument(); //create empty MutableDocument to retrieve students id
     final student = Student(document.id, firstName: firstName, lastName: lastName,
         classLevel: classLevel, classChar: classChar,
         trainingDirections: trainingDirections, books: []);
     database.updateDocFromEntity(student, document);
-    await database.saveDocument(document);
+    return document;
   }
 
-  Stream<List<Student>> streamStudents (String? ftsQuery, Filter? filterOptions) async*{
-      String query = """SELECT META().id, ${TextRes.studentFirstNameJson}, 
-      ${TextRes.studentLastNameJson}, ${TextRes.studentClassLevelJson}, 
-      ${TextRes.studentClassCharJson}, ${TextRes.studentTrainingDirectionsJson},
-      ${TextRes.studentBooksJson} FROM _""";
+  Stream<List<Student>> streamStudents (String? ftsQuery, Filter? filterOptions) async* {
+      String query = BuildQuery.buildStudentListQuery(ftsQuery, filterOptions);
 
-      //TODO add the functionality to build the query
+      yield* database.streamLiveDocs(query).asyncMap((change) {
+        return change.results
+            .asStream()
+            .map((result) => database.toEntity(Student.fromJson, result))
+            .toList();
+      });
   }
 
+
+  Future<void> deleteStudent (Student student) async{
+    final doc = await database.getDoc(student.id);
+    if (doc != null) database.deleteDoc(doc);
+  }
 
 
 
