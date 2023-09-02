5eb4a6398081eccaf48898ade29082ee1af52b89
ManagedStudios <dibbomrinmoy@hotmail.com>
2023-09-02 22:25:49 +0200

    class to build queries for couchbase

---
+++ lib/Data/buildQuery.dart
@@ -0,0 +1,16 @@
+
+import 'package:buecherteam_2023_desktop/Data/filter.dart';
+import 'package:buecherteam_2023_desktop/Resources/text.dart';
+
+class BuildQuery {
+
+  static String buildStudentListQuery (String? ftsQuery, Filter? filterOptions) {
+    String query = """SELECT META().id, ${TextRes.studentFirstNameJson}, 
+      ${TextRes.studentLastNameJson}, ${TextRes.studentClassLevelJson}, 
+      ${TextRes.studentClassCharJson}, ${TextRes.studentTrainingDirectionsJson},
+      ${TextRes.studentBooksJson} FROM _ 
+      WHERE ${TextRes.typeJson}='${TextRes.studentTypeJson}' """;
+
+    return query;
+  }
+}
