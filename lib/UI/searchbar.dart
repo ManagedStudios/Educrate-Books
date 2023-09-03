d989e1e7a12ffead555f2ca7799d1a1a41c98063
ManagedStudios <dibbomrinmoy@hotmail.com>
2023-09-03 19:31:05 +0200

    comments and tests

--- lib/UI/searchbar.dart
+++ lib/UI/searchbar.dart
@@ -7,18 +7,18 @@ class LfgSearchbar extends StatelessWidget {
   LfgSearchbar({super.key, required this.onChangeText, required this.amountOfFilteredStudents});
 
   final Function(String search) onChangeText;
-  final int amountOfFilteredStudents;
+  final int amountOfFilteredStudents; //amount of students that are retrieved after applying filters
 
   final TextEditingController textEditingController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Expanded(
+        Expanded( //searchBars width depends on margins and other widget - same as studentCard
           child: SearchBar(
             controller: textEditingController,
             hintText: TextRes.studentSearchHint,
-            elevation: const MaterialStatePropertyAll(4),
+            elevation: const MaterialStatePropertyAll(Dimensions.elevationMedium),
             textStyle: MaterialStatePropertyAll(Theme.of(context).textTheme.labelMedium),
             hintStyle: MaterialStatePropertyAll(Theme.of(context).textTheme.labelMedium
                 ?.copyWith(color: Theme.of(context).colorScheme.onSurfaceVariant)),
@@ -26,15 +26,15 @@ class LfgSearchbar extends StatelessWidget {
               padding: EdgeInsets.all(Dimensions.paddingSmall),
               child: Icon(Icons.search, size: 28,),
             ),
-            trailing: [Padding(
+            trailing: [Padding( //trailing displays the amount of students
               padding: const EdgeInsets.all(Dimensions.paddingSmall),
               child: Text("$amountOfFilteredStudents ${TextRes.student}",
                 style: Theme.of(context).textTheme.labelMedium?.copyWith(color: Theme.of(context).colorScheme.surfaceTint)),
             )],
             surfaceTintColor: MaterialStatePropertyAll(Theme.of(context).colorScheme.surface),
             overlayColor: MaterialStatePropertyAll(Theme.of(context).colorScheme.surfaceVariant),
-            onSubmitted: (text) => textEditingController.clear(),
-            onChanged: onChangeText,
+            onSubmitted: (text) => textEditingController.clear(), //on enter searchbar should be cleared
+            onChanged: onChangeText, //search while typing
           ),
         ),
       ],
---
+++ test/searchbar_test.dart
@@ -0,0 +1,78 @@
+
+
+import 'package:buecherteam_2023_desktop/Resources/text.dart';
+import 'package:buecherteam_2023_desktop/Theme/color_scheme.dart';
+import 'package:buecherteam_2023_desktop/Theme/text_theme.dart';
+import 'package:buecherteam_2023_desktop/UI/searchbar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockFunction extends Mock {
+  void onChangeText(String text);
+}
+
+void main () {
+
+  late MockFunction mockFunction;
+  late int amountOfFilteredStudents;
+
+  setUp(() {
+    mockFunction = MockFunction();
+    amountOfFilteredStudents = 0;
+  });
+
+  Widget createWidgetUnderTest () {
+    return MaterialApp(
+      theme: ThemeData(
+          colorScheme: lightColorScheme,
+          useMaterial3: true,
+          fontFamily: 'Helvetica Neue',
+          textTheme: textTheme
+      ),
+      home: LfgSearchbar(
+        onChangeText: mockFunction.onChangeText,
+        amountOfFilteredStudents: amountOfFilteredStudents,
+      ),
+    );
+  }
+
+  testWidgets("Test if widget renders correctly", (tester) async{
+    await tester.pumpWidget(createWidgetUnderTest());
+    expect(find.byType(LfgSearchbar), findsOneWidget);
+  });
+
+  testWidgets("Test if onChangeText is triggered when typing", (tester) async{
+    String typedValue = "hello";
+    await tester.pumpWidget(createWidgetUnderTest());
+    final textField = find.byType(TextField);
+    expect(textField, findsOneWidget);
+    await tester.enterText(textField, typedValue);
+    await tester.pumpAndSettle();
+    verify(() => mockFunction.onChangeText(typedValue)).called(1);
+  });
+
+  testWidgets("Test if amount of Students is displayed", (tester) async{
+    await tester.pumpWidget(createWidgetUnderTest());
+    expect(find.text("$amountOfFilteredStudents ${TextRes.student}"), findsOneWidget);
+  });
+  
+  testWidgets("Searchbar is cleared on enter", (tester) async{
+    await tester.pumpWidget(createWidgetUnderTest());
+
+    final Finder finderTextField = find.byType(TextField);
+
+    // Simulate typing into the TextField
+    await tester.enterText(finderTextField, "John");
+    await tester.pump();
+
+    expect(find.text("John"), findsOneWidget);
+    
+    await tester.testTextInput.receiveAction(TextInputAction.done);
+    await tester.pump();
+    
+    expect(find.text(""), findsOneWidget);
+    
+  });
+
+}
