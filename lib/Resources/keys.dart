a0a12838d8fabfddebd18a1b11e57d2a5943aee0
ManagedStudios <dibbomrinmoy@hotmail.com>
2023-09-07 17:25:13 +0200

    central ressource to control keyValues of widgets

---
+++ lib/Resources/keys.dart
@@ -0,0 +1,13 @@
+
+class Keys {
+  static String ActionDropdownAvailableContainerSizedBoxKey = "ActionAvConSB";
+
+  static String ActionDropdownAvailableCardKey = "ActionAvCard";
+
+  static String ActionDropdownSelectedWrapCard = "ActionSeWrCard";
+
+  static String ActionDropdownSelectedWrapSizedBoxForCard = "ActionSeWrSBFCard";
+
+  static String ActionDropdownSelectedWrapSizedBoxForTextfield = "ActionSeWrFTf";
+
+}
