78f39b8bc7aa87707f96c4de68c9fe9c3629d8d2
ManagedStudios <dibbomrinmoy@hotmail.com>
2023-09-07 17:28:39 +0200

    finished dropdown with tests

---
+++ lib/UI/tag_dropdown/dropdown.dart
@@ -0,0 +1,169 @@
+import 'package:buecherteam_2023_desktop/Data/lfg_chip.dart';
+
+import 'package:buecherteam_2023_desktop/UI/tag_dropdown/action_dropdown.dart';
+import 'package:buecherteam_2023_desktop/UI/tag_dropdown/chip_wrap.dart';
+import 'package:flutter/material.dart';
+
+
+/*
+Dropdown is the entry point for Dropdowns.
+The dropdown consists of a selection view that opens on click a actionDropdown Overlay
+to change the selection.
+Dropdown is self contained and updates its state within the widget, but it also
+notifies parents about state changes to make updates to databases and similar.
+
+The Dropdown requires a type implementing LfgChip. Its similar to Item of the Material.io
+Dropdown, but with the benefit that you can cast without any worry the delegated Results
+of onDeleteChip and onAddChip to the initial data objects.
+ */
+class Dropdown<T extends LfgChip> extends StatefulWidget {
+  const Dropdown({super.key, required this.availableChips,
+    required this.selectedChips, required this.onAddChip, required this.onDeleteChip,
+    required this.multiSelect, required this.width, this.hintText,
+    required this.onCloseOverlay});
+
+  final List<LfgChip> selectedChips;
+  final List<LfgChip> availableChips;
+  final Function(LfgChip chip) onAddChip;
+  final Function(LfgChip chip) onDeleteChip;
+  final Function (List<LfgChip> selectedChips) onCloseOverlay;
+  final bool multiSelect;
+  final double width;
+  final String? hintText;
+
+  @override
+  State<Dropdown> createState() => _DropdownState();
+}
+
+class _DropdownState<T extends LfgChip> extends State<Dropdown<T>> {
+  bool isOverlayOpen = false;
+  late GlobalKey wrapChipsField;
+  late List<LfgChip> selectedChips;
+  late List<LfgChip> availableChips;
+  OverlayEntry? overlayEntry;
+
+  @override
+  void initState() {
+    super.initState();
+    wrapChipsField = GlobalKey(); //globalKey used to get the position of the selection view
+    selectedChips = widget.selectedChips; //initial state that is updated internally
+    availableChips = widget.availableChips;
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    overlayEntry?.remove();
+    overlayEntry?.dispose();
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return IgnorePointer(
+      ignoring: isOverlayOpen, //disable interaction when overlay is open
+      child: ChipWrap(key: wrapChipsField, chips: selectedChips,
+            onClickChipRow: (_) {
+              showOverlay();
+              widget.onCloseOverlay(selectedChips);
+            },
+            width: widget.width),
+    );
+  }
+
+
+  void showOverlay() {
+    final overlay = Overlay.of(context);
+    RenderBox renderBox = wrapChipsField.currentContext?.findRenderObject() as RenderBox;
+    final offset = renderBox.localToGlobal(const Offset(0, -32)); //retrieve position of textField
+    overlayEntry??{ //if overlayEntry is not initialized create a new one (case when you open the overlay)
+      createOverlayEntry(offset),
+      overlay.insert(overlayEntry!)
+    };
+    setState(() {
+      isOverlayOpen = true;
+    });
+
+  }
+
+  void createOverlayEntry (Offset offset) {
+    overlayEntry = OverlayEntry(
+      builder: (context) => Positioned( //position the Overlay widget correctly
+        left: offset.dx,
+        top: offset.dy,
+        child: TapRegion(
+          onTapOutside: (_) => closeOverlay(),
+          child: ActionDropdown(width: widget.width, selectedChips: selectedChips,
+              hintText: widget.hintText,
+              onDeleteChip: (chip) {
+                  widget.onDeleteChip(chip); //notify parents about changes
+                  deleteChip(chip); //internally update State
+              },
+              availableChips: availableChips,
+              onAddChip: (chip) {
+                widget.onAddChip(chip);
+                addChip(chip);
+              }),
+        )
+      ),
+    );
+  }
+
+  /*
+  Overlays don't react to setState so you have to
+  manually determine when to update the overlay
+   */
+
+  void markOverlayReadyForUpdate() {
+    if (overlayEntry != null) {
+      overlayEntry!.markNeedsBuild();
+    }
+  }
+
+  void closeOverlay() {
+    if(overlayEntry != null) {
+      overlayEntry!.remove();
+      overlayEntry = null;
+      setState(() {
+        isOverlayOpen = false;
+      });
+    }
+  }
+
+  void addChip(LfgChip chip) {
+    markOverlayReadyForUpdate(); //update Overlay on next state change occurring right in addChip
+    if(widget.multiSelect) {
+      setState(() {
+        selectedChips.add(chip);
+        availableChips.removeWhere((element) => element.getLabelText()==chip.getLabelText());
+      });
+    } else {
+      setState(() {
+        /*
+        first move all chips (usually should be 0 or 1 element) to availableChips and then delete
+         */
+        selectedChips.forEach((element) => availableChips.add(element));
+        selectedChips.removeRange(0, selectedChips.length);
+
+        /*
+        then add the chip and remove from available
+         */
+        selectedChips.add(chip);
+        availableChips.removeWhere((element) => element.getLabelText()==chip.getLabelText());
+      });
+    }
+
+  }
+
+  void deleteChip(LfgChip chip) {
+    markOverlayReadyForUpdate();
+      setState(() {
+        availableChips.add(chip);
+        selectedChips.removeWhere((element) => element.getLabelText()==chip.getLabelText());
+      });
+
+  }
+
+
+}
---
+++ test/tag_dropdown/dropdown_test.dart
@@ -0,0 +1,252 @@
+
+import 'package:buecherteam_2023_desktop/Data/lfg_chip.dart';
+import 'package:buecherteam_2023_desktop/Data/training_directions_data.dart';
+import 'package:buecherteam_2023_desktop/Theme/color_scheme.dart';
+import 'package:buecherteam_2023_desktop/Theme/text_theme.dart';
+import 'package:buecherteam_2023_desktop/UI/tag_dropdown/action_dropdown.dart';
+import 'package:buecherteam_2023_desktop/UI/tag_dropdown/action_dropdown_available_container.dart';
+import 'package:buecherteam_2023_desktop/UI/tag_dropdown/action_dropdown_selected_wrap.dart';
+import 'package:buecherteam_2023_desktop/UI/tag_dropdown/chip_tag.dart';
+import 'package:buecherteam_2023_desktop/UI/tag_dropdown/chip_wrap.dart';
+import 'package:buecherteam_2023_desktop/UI/tag_dropdown/dropdown.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockFunctions extends Mock{
+  void onAddChip(LfgChip chip);
+  void onDeleteChip(LfgChip chip);
+  void onCloseOverlay(List<LfgChip> chips);
+}
+
+void main () {
+  late List<LfgChip> availableChips;
+  late List<LfgChip> selectedChips;
+  late MockFunctions mockFunctions;
+  setUp(() {
+    mockFunctions = MockFunctions();
+    String strA = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod ETH-0";
+    String strS = "Stet clita kasd gubergren";
+    availableChips = strA.split(" ").map((e) => TrainingDirectionsData(e, 0)).toList();
+    selectedChips = strS.split(" ").map((e) => TrainingDirectionsData(e, 0)).toList();
+  });
+
+  Widget createWidgetUnderTest (List<LfgChip> availableChipList,
+      List<LfgChip> selectedChipList, {double width = 400, String? hintText,
+      bool multiSelect = true}) {
+    return MaterialApp(
+        theme: ThemeData(
+            colorScheme: lightColorScheme,
+            useMaterial3: true,
+            fontFamily: 'Helvetica Neue',
+            textTheme: textTheme
+        ),
+        home: Padding(
+          padding: EdgeInsets.all(30),
+          child: Dropdown(availableChips: availableChipList,
+          selectedChips: selectedChipList,
+    onAddChip: mockFunctions.onAddChip,
+    onDeleteChip: mockFunctions.onDeleteChip,
+    multiSelect: multiSelect, width: width,
+    onCloseOverlay: mockFunctions.onCloseOverlay),
+        )
+    );
+  }
+
+  testWidgets('Tapping on ChipWrap opens ActionDropdown overlay', (WidgetTester tester) async {
+    // Load the main widget
+    await tester.pumpWidget(createWidgetUnderTest(availableChips, selectedChips));
+    // Find the ChipWrap widget
+    final chipWrapFinder = find.byType(ChipWrap);
+
+    // Simulate a tap on ChipWrap
+    await tester.tap(chipWrapFinder);
+
+    // Call pump to trigger a rebuild
+    await tester.pumpAndSettle();
+
+    // Check if the ActionDropdown overlay is present in the widget tree
+    expect(find.byType(ActionDropdown), findsOneWidget);
+  });
+
+  testWidgets('Clicking outside of ActionDropdown overlay closes it', (WidgetTester tester) async {
+    // Load the main widget
+    await tester.pumpWidget(createWidgetUnderTest(availableChips, selectedChips));
+
+    // Find the ChipWrap widget
+    final chipWrapFinder = find.byType(ChipWrap);
+
+    // Simulate a tap on ChipWrap to open the overlay
+    await tester.tap(chipWrapFinder);
+
+    // Call pump to trigger a rebuild
+    await tester.pumpAndSettle();
+
+    // Verify the ActionDropdown overlay is present in the widget tree
+    expect(find.byType(ActionDropdown), findsOneWidget);
+
+    // Now, simulate tapping outside to close the overlay
+    await tester.tapAt(Offset(0, 0)); // tapping at the top-left corner
+    await tester.pumpAndSettle();
+
+    // Verify the ActionDropdown overlay is no longer present in the widget tree
+    expect(find.byType(ActionDropdown), findsNothing);
+  });
+
+  testWidgets('Overlay is positioned correctly relative to ChipWrap', (WidgetTester tester) async {
+    // Load the main widget
+    await tester.pumpWidget(createWidgetUnderTest(availableChips, selectedChips));
+
+    // Find the ChipWrap widget
+    final chipWrapFinder = find.byType(ChipWrap);
+    // Fetch positions of ChipWrap and ActionDropdown overlay
+    final chipWrapBox = tester.renderObject<RenderBox>(chipWrapFinder);
+    final chipWrapOffset = chipWrapBox.localToGlobal(Offset.zero);
+
+    // Simulate a tap on ChipWrap to open the overlay
+    await tester.tap(chipWrapFinder);
+
+    // Call pump to trigger a rebuild
+    await tester.pump();
+
+
+    final actionDropdownFinder = find.byType(ActionDropdown);
+    final actionDropdownBox = tester.renderObject<RenderBox>(actionDropdownFinder);
+    final actionDropdownOffset = actionDropdownBox.localToGlobal(Offset.zero);
+
+    // Verify that overlay's top-left corner's position matches ChipWrap's (or however you've defined its position)
+    // This assumes the overlay is positioned right below the ChipWrap, but you may adjust accordingly
+    expect(actionDropdownOffset.dx, chipWrapOffset.dx);
+    expect(actionDropdownOffset.dy, closeTo(chipWrapOffset.dy - 32, 4.0)); // We use closeTo here to account for potential small variations
+
+  });
+
+  testWidgets('Selecting a chip in the overlay calls onAddChip and updates state', (WidgetTester tester) async {
+    // Load the main widget
+    await tester.pumpWidget(createWidgetUnderTest(availableChips, selectedChips));
+
+    // Find the ChipWrap widget and open the overlay
+    final chipWrapFinder = find.byType(ChipWrap);
+    await tester.tap(chipWrapFinder);
+    await tester.pump();
+
+    // Find the first chip in the ActionDropdownAvailableContainer and tap it
+    final firstAvailableChipFinder = find.descendant(
+      of: find.byType(ActionDropdownAvailableContainer),
+      matching: find.byType(ChipTag),
+    ).first;
+
+    final chipContent = (firstAvailableChipFinder.evaluate().first.widget as ChipTag).chipContent;
+
+    await tester.tap(firstAvailableChipFinder);
+    await tester.pump();
+
+    // Verify the onAddChip mock method was called with the appropriate chip
+    verify(() => mockFunctions.onAddChip(chipContent)).called(1);
+
+    // Verify that the internal state has been updated
+    // This assumes you have a way to access the current state of the widget or can determine it by other means
+    // For instance, the chip should no longer be in the availableChips and should be in the selectedChips
+
+    expect(availableChips.contains(chipContent), isFalse);
+    expect(selectedChips.contains(chipContent), isTrue);
+  });
+
+  testWidgets('When multiSelect is false, selecting a new chip deselects the previously selected chip', (WidgetTester tester) async {
+    // Load the main widget with multiSelect set to false
+    await tester.pumpWidget(createWidgetUnderTest(availableChips, selectedChips, multiSelect: false));
+
+    // Verify that there is initially a chip in the selectedChips
+    final initialSelectedChip = selectedChips.first;
+    expect(selectedChips.contains(initialSelectedChip), isTrue);
+    expect(availableChips.contains(initialSelectedChip), isFalse);
+
+    // Find the ChipWrap widget and open the overlay
+    final chipWrapFinder = find.byType(ChipWrap);
+    await tester.tap(chipWrapFinder);
+    await tester.pump();
+
+    // Find the first chip in the ActionDropdownAvailableContainer (which should be different from the initialSelectedChip) and tap it
+    final firstAvailableChipFinder = find.descendant(
+      of: find.byType(ActionDropdownAvailableContainer),
+      matching: find.byType(ChipTag),
+    ).first;
+
+    final newChipContent = (firstAvailableChipFinder.evaluate().first.widget as ChipTag).chipContent;
+
+    await tester.tap(firstAvailableChipFinder);
+    await tester.pump();
+
+    // Verify that the initially selected chip is back in the availableChips and the new chip is now in the selectedChips
+    expect(selectedChips.contains(initialSelectedChip), isFalse);
+    expect(availableChips.contains(initialSelectedChip), isTrue);
+    expect(selectedChips.contains(newChipContent), isTrue);
+    expect(availableChips.contains(newChipContent), isFalse);
+  });
+
+  testWidgets('Deleting a chip in the overlay triggers onDeleteChip callback and updates internal state', (WidgetTester tester) async {
+    // Load the main widget
+    await tester.pumpWidget(createWidgetUnderTest(availableChips, selectedChips));
+
+    // Verify that there is initially a chip in the selectedChips
+    final initialSelectedChip = selectedChips.first;
+    expect(selectedChips.contains(initialSelectedChip), isTrue);
+    expect(availableChips.contains(initialSelectedChip), isFalse);
+
+    // Open the overlay by tapping the ChipWrap
+    final chipWrapFinder = find.byType(ChipWrap);
+    await tester.tap(chipWrapFinder);
+    await tester.pump();
+
+    // Find a deletable chip within the overlay's ActionDropdownSelectedWrap and delete it
+    final deleteIconFinder = find.descendant(
+      of: find.byType(ActionDropdownSelectedWrap),
+      matching: find.byIcon(Icons.close), // Assuming Icons.close is the delete icon in ChipTag
+    ).first;
+
+    await tester.tap(deleteIconFinder);
+    await tester.pump();
+
+    // Check if onDeleteChip was called with the right chip
+    verify(() => mockFunctions.onDeleteChip(initialSelectedChip)).called(1);
+
+    // Verify that the chip is now in the list of availableChips and not in the selectedChips anymore
+    expect(selectedChips.contains(initialSelectedChip), isFalse);
+    expect(availableChips.contains(initialSelectedChip), isTrue);
+  });
+
+  testWidgets('Adding and removing a chip updates availableChips and selectedChips correctly', (WidgetTester tester) async {
+    // Load the main widget
+    await tester.pumpWidget(createWidgetUnderTest(availableChips, selectedChips));
+
+    // Open the overlay by tapping the ChipWrap
+    final chipWrapFinder = find.byType(ChipWrap);
+    await tester.tap(chipWrapFinder);
+    await tester.pump();
+
+    // Add a chip from availableChips
+    final chipToAdd = availableChips.first;
+    final chipToAddFinder = find.text(chipToAdd.getLabelText()).first; // Assuming the chip's display is its label text
+    await tester.tap(chipToAddFinder);
+    await tester.pump();
+
+    expect(selectedChips.contains(chipToAdd), isTrue);
+    expect(availableChips.contains(chipToAdd), isFalse);
+
+    // Delete a chip from selectedChips
+    final chipToDelete = selectedChips.first;
+    final deleteIconFinder = find.descendant(
+      of: find.byKey(Key(chipToDelete.getLabelText())),
+      matching: find.byIcon(Icons.close), // Assuming Icons.close is the delete icon in ChipTag
+    ).first;
+    await tester.tap(deleteIconFinder);
+    await tester.pump();
+
+    expect(selectedChips.contains(chipToDelete), isFalse);
+    expect(availableChips.contains(chipToDelete), isTrue);
+  });
+
+
+
+
+}
