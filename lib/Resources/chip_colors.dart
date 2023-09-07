9c074d750a229b122e9ac8ecb4fca59b02605a6a
ManagedStudios <dibbomrinmoy@hotmail.com>
2023-09-07 17:25:37 +0200

    color variety for chips

---
+++ lib/Resources/chip_colors.dart
@@ -0,0 +1,19 @@
+
+import 'dart:ui';
+
+class ChipColors {
+  static const Color lightGray = Color(0xFFF1F0F0);
+  static const Color gray = Color(0xFFE3E2E0);
+  static const Color orange = Color(0xFFF5DFCC);
+  static const Color yellow = Color(0xFFFAEDCC);
+  static const Color green = Color(0xFFDEECDC);
+  static const Color blue = Color(0xFFD6E4EE);
+  static const Color purple = Color(0xFFE6DEED);
+  static const Color pink = Color(0xFFF1E1E9);
+  static const Color red = Color(0xFFF7E0DB);
+
+  static const List<Color> chipColors = [
+    gray, orange, yellow, green, blue, purple, pink, red
+  ];
+
+}
