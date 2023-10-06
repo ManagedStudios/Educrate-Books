bc7c1eccfca28fd43944138ebf1ea0285686c8a4
ManagedStudios <dibbomrinmoy@hotmail.com>
2023-10-06 18:06:36 +0200

    sigmoid margins

--- lib/UI/student_view.dart
+++ lib/UI/student_view.dart
@@ -1,5 +1,7 @@
 
-import 'package:buecherteam_2023_desktop/Data/class_data.dart';
+import 'dart:math';
+
+
 import 'package:buecherteam_2023_desktop/UI/all_students_column.dart';
 import 'package:flutter/material.dart';
 
@@ -15,15 +17,20 @@ class StudentView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final double marginWidth =
-    MediaQuery.of(context).size.width*0.15>Dimensions.minMarginStudentView?MediaQuery.of(context).size.width*0.15:Dimensions.minMarginStudentView;
+    final mediaQuery = MediaQuery.of(context).size;
+    final double marginWidth = mediaQuery.width * (0.03+0.12/(1+pow(2.71, -0.005*(mediaQuery.width-1150))));
     return Padding(
       padding: const EdgeInsets.only(top: Dimensions.paddingVeryBig),
       child: Row(
         children: [
           SizedBox(width: marginWidth,),
-          Expanded(child: AllStudentsColumn()),
-          SizedBox(width: Dimensions.spaceMedium,),
+          const Expanded(child: AllStudentsColumn()),
+          SizedBox(width: Dimensions.spaceMedium,), //TODO make sizedbox width dynamic
+          Container(
+            width: Dimensions.lineWidth,
+            height: MediaQuery.of(context).size.height*0.7,
+            color: Theme.of(context).colorScheme.outline,
+          ),
           Expanded(child: Container()), //studentDetail
           SizedBox(width: marginWidth,)
         ],
