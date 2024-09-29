397da6d0db1128da471289a68369b0182bb9cebc
ManagedStudios <dibbomrinmoy@hotmail.com>
2024-09-29 09:02:40 +0200

    feat: added more colors

--- lib/Resources/chip_colors.dart
+++ lib/Resources/chip_colors.dart
@@ -10,6 +10,16 @@ class ChipColors {
   static const Color purple = Color(0xFFE6DEED);
   static const Color pink = Color(0xFFF1E1E9);
   static const Color red = Color(0xFFF7E0DB);
+  static const Color mintGreen = Color(0xFFDAF5E6);
+  static const Color lavender = Color(0xFFEDE4F5);
+  static const Color peach = Color(0xFFFDE5D6);
+  static const Color lightCoral = Color(0xFFFADDD6);
+  static const Color skyBlue = Color(0xFFE4F2F9);
+  static const Color paleYellow = Color(0xFFFFF5D4);
+  static const Color softTeal = Color(0xFFD7F0EF);
+  static const Color butterCream = Color(0xFFFFF2E0);
+  static const Color babyPink = Color(0xFFFDE6F2);
+  static const Color powderBlue = Color(0xFFE2EDF7);
 
   static const List<Color> chipColors = [
     gray,
@@ -19,6 +29,16 @@ class ChipColors {
     blue,
     purple,
     pink,
-    red
+    red,
+    mintGreen,
+    lavender,
+    peach,
+    lightCoral,
+    skyBlue,
+    paleYellow,
+    softTeal,
+    butterCream,
+    babyPink,
+    powderBlue
   ];
 }
