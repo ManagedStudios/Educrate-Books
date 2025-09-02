eb145cc843ae89888f4879b6fde8a02aac9896b1
ManagedStudios <dibbomrinmoy@hotmail.com>
2025-09-02 09:06:38 +0200

    feat: scaffold for Sync_Parent

---
+++ lib/UI/settings_dialog/sync_parent.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SyncParent extends StatelessWidget {
+  const SyncParent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+
+      ],
+    );
+  }
+}
