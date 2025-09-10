48c9adabed616d8d234d7d0faf4a41acbc3a7ade
ManagedStudios <dibbomrinmoy@hotmail.com>
2025-09-10 20:16:53 +0200

    fix: ready for production

--- android/app/src/main/kotlin/com/example/buecherteam_2023_desktop/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.buecherteam_2023_desktop
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/UI/mobile/class_view.dart
+++ lib/UI/mobile/class_view.dart
@@ -5,6 +5,7 @@ import 'package:buecherteam_2023_desktop/UI/mobile/classes/classes_row.dart';
 import 'package:buecherteam_2023_desktop/UI/mobile/student_list_view.dart';
 import 'package:buecherteam_2023_desktop/Util/database/getter.dart';
 import 'package:buecherteam_2023_desktop/Util/transformer/grouper.dart';
+import 'package:cbl/cbl.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -19,7 +20,7 @@ class ClassView extends StatelessWidget {
     return FutureBuilder(
         future: getAllClasses(DB()),
         builder: (context, data) {
-          if (!data.hasData) return const CircularProgressIndicator();
+          if (!data.hasData || DB().replicatorStatus.value?.activity == ReplicatorActivityLevel.busy) return const CircularProgressIndicator();
           final classRows = groupClassesByLevel(data.data!);
 
           return ListView(
